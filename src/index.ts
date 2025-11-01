4d0f4653cb84fdf06d07677499d4e84adf7e98db
Vince Au <vinceau09@gmail.com>
2025-11-01 22:42:56 +1100

    fix export for webpack

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export { i18nextTranslationLoader } from "./loaders/i18nextTranslationLoader";
-export { I18nEmitPlugin } from "./plugins/i18nEmitPlugin";
+import { i18nextTranslationLoader } from "./loaders/i18nextTranslationLoader";
+import { I18nEmitPlugin } from "./plugins/i18nEmitPlugin";
+
+// Webpack loader compatibility: Export loader as default, plugin as property
+module.exports = i18nextTranslationLoader;
+module.exports.I18nEmitPlugin = I18nEmitPlugin;
+module.exports.i18nextTranslationLoader = i18nextTranslationLoader;
+
+// Also provide TypeScript-friendly named exports for better DX
+export { i18nextTranslationLoader, I18nEmitPlugin };
