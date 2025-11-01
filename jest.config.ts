158091e644797b089cdad894e5cbbf68eaecd8d9
Vince Au <vinceau09@gmail.com>
2025-11-01 16:47:52 +1100

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,24 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: { '^.+\\.ts$': 'ts-jest' },
-  testMatch: ['**/*.test.ts'],
-  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+  testMatch: ['<rootDir>/src/**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/**/tests/**',
+    '!src/declarations.d.ts'
+  ],
   coverageDirectory: 'coverage',
   verbose: true,
-  clearMocks: true
+  clearMocks: true,
+  // Support absolute imports and path mapping
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
 
 export default config;
