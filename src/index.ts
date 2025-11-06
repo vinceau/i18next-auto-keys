7ca15e7ecfd2c80f0a1098bbb04e39ca3060dcbf
Vince Au <vinceau09@gmail.com>
2025-11-06 12:17:54 +1100

    use config in cli tools

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { i18nextAutoKeyLoader } from "./loaders/i18nextAutoKeyLoader";
 import { I18nextAutoKeyEmitPlugin } from "./plugins/i18nextAutoKeyEmitPlugin";
 import { createI18nextAutoKeyTransformerFactory } from "./transformers/i18nextAutoKeyTransformer";
 import { i18nStore } from "./common/i18nStore";
+import { loadConfig } from "./common/config/loadConfig";
 
 // Webpack loader compatibility: Export loader as default, plugin as property
 module.exports = i18nextAutoKeyLoader;
@@ -11,6 +12,7 @@ module.exports.i18nextAutoKeyLoader = i18nextAutoKeyLoader;
 // Shared functionality for CLI and advanced users
 module.exports.createI18nextAutoKeyTransformerFactory = createI18nextAutoKeyTransformerFactory;
 module.exports.i18nStore = i18nStore;
+module.exports.loadConfig = loadConfig;
 
 // Also provide TypeScript-friendly named exports for better DX
-export { i18nextAutoKeyLoader, I18nextAutoKeyEmitPlugin, createI18nextAutoKeyTransformerFactory, i18nStore };
+export { i18nextAutoKeyLoader, I18nextAutoKeyEmitPlugin, createI18nextAutoKeyTransformerFactory, i18nStore, loadConfig };
